b6b67144d7062bd60dbbe6bcc9689d1589302c53
DragosU25 <149568004+DragosU25@users.noreply.github.com>
2024-08-18 21:41:41 +0200

    pregatire

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addContact } from '../../redux/contacts/contactsSlice';
+import { getFilteredContacts } from '../../redux/contacts/contactsSelector';
+import styles from './ContactForm.module.css';
+import { nanoid } from '@reduxjs/toolkit';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const dispatch = useDispatch();
+  const contacts = useSelector(getFilteredContacts);
+
+  const handleChange = event => {
+    const { name, value } = event.target;
+    if (name === 'name') setName(value);
+    if (name === 'number') setNumber(value);
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    // Verifică dacă contactul există deja
+    const contactExists = contacts.some(
+      contact => contact.name.toLowerCase() === name.toLowerCase()
+    );
+
+    if (contactExists) {
+      alert(`Contact with name "${name}" already exists.`);
+      return;
+    }
+
+    const newContact = {
+      id: nanoid(), // Creează un id unic
+      name,
+      number,
+    };
+
+    dispatch(addContact(newContact)); // Adaugă contactul în Redux
+
+    // Resetează formularul și state-ul local
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <label className={styles.formLabel}>Name</label>
+      <input
+        type="text"
+        name="name"
+        value={name}
+        onChange={handleChange}
+        required
+        className={styles.formInput}
+      />
+      <label className={styles.formLabel}>Number</label>
+      <input
+        type="number"
+        name="number"
+        value={number}
+        onChange={handleChange}
+        required
+        className={styles.formInput}
+      />
+      <button type="submit" className={styles.submitButton}>
+        Add Contact
+      </button>
+    </form>
+  );
+}
+
+export default ContactForm;
